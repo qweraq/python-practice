7dc8b176c09d5af731c232a75f2ab31df7310613
qweraq <61075315+qweraq@users.noreply.github.com>
2020-08-04 11:03:53 +0800

    获取cpu速度
    判断系统类型，获取返回cpu的速度

--- cpu_speed.py
+++ cpu_speed.py
@@ -1,8 +1,12 @@
+"""
+mac系统和windows系统的代码没有经过实际运行，并不知道是否能成功
+"""
 import platform
 import subprocess
 import fileinput
 
 
+#获得mac系统cpu的速度
 def get_mac_cpu_speed():
     commond = 'system_profiler SPHardwareDataType | grep "Processor Speed" | cut -d ":" -f2'
     proc = subprocess.Popen([commond], shell=True, stdout=subprocess.PIPE)
@@ -12,6 +16,7 @@ def get_mac_cpu_speed():
     return speed
 
 
+#获取Linux系统cpu的速度
 def get_linux_cpu_speed():
     for line in fileinput.input('/proc/cpuinfo'):
         if 'MHz' in line:
@@ -21,6 +26,16 @@ def get_linux_cpu_speed():
             return "{speed} GHz".format(speed=speed)
 
 
+#获取windows系统cpu的速度
+def get_windows_cpu_speed():
+    import winreg
+    key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"HARDWARE\DESCRIPTION\System\CentralProcessor\0")
+    speed, type = winreg.QueryValueEx(key, "-MHz")
+    speed = round(float(speed) / 1024, 1)
+    return "{speed} GHz".format(speed=speed)
+
+
+#判断系统的类型，并返回正确的值
 def get_cpu_speed():
     osname = platform.system()
     speed = ''
@@ -29,7 +44,7 @@ def get_cpu_speed():
     if osname == "Linux":
         speed = get_linux_cpu_speed()
     if osname in ["Windows", "Win32"]:
-        print("Sorry, I can't get cpu's speed")
+        speed = get_windows_cpu_speed()
     return speed
 
 
