29cef9dd17d90f866d2bc6bbefeafd07643ad412
qweraq <61075315+qweraq@users.noreply.github.com>
2020-08-04 10:37:10 +0800

    搜索大文件
    通过os.path()函数，搜索指定目录下的文件，并列出超过指定大小的文件路径和文件。

--- bigfile.py
+++ bigfile.py
@@ -1,18 +1,19 @@
+#该代码未能在windows10系统运行成功，估计是路径格式问题
 import os
 
 
 def get_big_file(path, filesize):
-    for dirpath, dirnames, filenames in os.walk(path):
+    for dirpath, dirnames, filenames in os.walk(path):  #遍历文件
         for filename in filenames:
             target_file = os.path.join(dirpath, filename)
-            if not os.path.isfile(target_file):
+            if not os.path.isfile(target_file):  #排除非文件类型
                 continue
             size = os.path.getsize(target_file)
             if size > filesize:
-                size = size//(1024 * 1024)
+                size = size//(1024 * 1024)  #转换文件大小的格式
                 size = '{size}M'.format(size=size)
                 print(target_file, size)
 
 
 if __name__ == '__main__':
-    get_big_file('/home/zmq', 100*1024*1024)
+    get_big_file('/home/zmq', 100*1024*1024)  #输入路径和文件大小并打印文件路径和文件大小
